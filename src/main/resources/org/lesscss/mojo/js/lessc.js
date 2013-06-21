de93e4f3e879da9a431f8ffabdba61358728a5aa
Trask Stalnaker <trask.stalnaker@gmail.com>
2013-06-20 23:18:48 -0500

    Add option to compile less files under Node.js
    Node.js is much much faster than Rhino.

---
+++ src/main/resources/org/lesscss/mojo/js/lessc.js
@@ -0,0 +1,21 @@
+var less = require('./less/index');
+var fs = require('fs');
+
+var inputFile = process.argv[2];
+var outputFile = process.argv[3];
+var compress = (process.argv[4] !== 'false');
+
+var inputText = fs.readFileSync(inputFile, 'utf8');
+var parser = new less.Parser();
+parser.parse(inputText, function(e, tree) {
+  if (e instanceof Object) {
+	throw e;
+  };
+  try {
+	  var result = tree.toCSS({compress: compress});
+      fs.writeFileSync(outputFile, result);
+  } catch (e) {
+	  fs.writeFileSync(outputFile, e.message);
+	  process.exit(1);
+  }
+});
