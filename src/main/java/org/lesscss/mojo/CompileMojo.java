82c67cb966b1526cf2e4b6a07a3278462bc71b9a
marco.semiao <marco.semiao@gmail.com>
2014-04-25 17:29:30 +0200

    bug fix with absolute uri IMPORT
    exemple :
    @import "/less/styles_variables.less";
    
    if the source file is not present into root directory

--- src/main/java/org/lesscss/mojo/CompileMojo.java
+++ src/main/java/org/lesscss/mojo/CompileMojo.java
@@ -35,156 +35,153 @@ import org.sonatype.plexus.build.incremental.BuildContext;
  */
 public class CompileMojo extends AbstractLessCssMojo {
 
-	/**
-	 * The directory for compiled CSS stylesheets.
-	 * 
-	 * @parameter expression="${lesscss.outputDirectory}" default-value="${project.build.directory}"
-	 * @required
-	 */
-	private File outputDirectory;
+  /**
+   * The directory for compiled CSS stylesheets.
+   * 
+   * @parameter expression="${lesscss.outputDirectory}" default-value="${project.build.directory}"
+   * @required
+   */
+  private File outputDirectory;
 
-	/**
-	 * When <code>true</code> the LESS compiler will compress the CSS stylesheets.
-	 * 
-	 * @parameter expression="${lesscss.compress}" default-value="false"
-	 */
-	private boolean compress;
+  /**
+   * When <code>true</code> the LESS compiler will compress the CSS stylesheets.
+   * 
+   * @parameter expression="${lesscss.compress}" default-value="false"
+   */
+  private boolean compress;
 
-	/**
-	 * The character encoding the LESS compiler will use for writing the CSS stylesheets.
-	 * 
-	 * @parameter expression="${lesscss.encoding}" default-value="${project.build.sourceEncoding}"
-	 */
-	private String encoding;
+  /**
+   * The character encoding the LESS compiler will use for writing the CSS stylesheets.
+   * 
+   * @parameter expression="${lesscss.encoding}" default-value="${project.build.sourceEncoding}"
+   */
+  private String encoding;
 
-	/**
-	 * When <code>true</code> forces the LESS compiler to always compile the LESS sources. By default LESS sources are only compiled when modified (including imports) or the CSS stylesheet does not exists.
-	 * 
-	 * @parameter expression="${lesscss.force}" default-value="false"
-	 */
-	private boolean force;
+  /**
+   * When <code>true</code> forces the LESS compiler to always compile the LESS sources. By default LESS sources are
+   * only compiled when modified (including imports) or the CSS stylesheet does not exists.
+   * 
+   * @parameter expression="${lesscss.force}" default-value="false"
+   */
+  private boolean force;
 
-	/**
-	 * The location of the LESS JavasSript file.
-	 * 
-	 * @parameter
-	 */
-	private File lessJs;
+  /**
+   * The location of the LESS JavasSript file.
+   * 
+   * @parameter
+   */
+  private File lessJs;
 
-	/**
-	 * The location of the NodeJS executable.
-	 *
-	 * @parameter
-	 */
-	private String nodeExecutable;
+  /**
+   * The location of the NodeJS executable.
+   * 
+   * @parameter
+   */
+  private String nodeExecutable;
 
-	/**
-	 * Execute the MOJO.
-	 * 
-	 * @throws MojoExecutionException
-	 *             if something unexpected occurs.
-	 */
-	public void execute() throws MojoExecutionException {
+  /**
+   * Execute the MOJO.
+   * 
+   * @throws MojoExecutionException if something unexpected occurs.
+   */
+  public void execute() throws MojoExecutionException {
 
-		long start = System.currentTimeMillis();
+    long start = System.currentTimeMillis();
 
-		if (getLog().isDebugEnabled()) {
-			getLog().debug("sourceDirectory = " + sourceDirectory);
-			getLog().debug("outputDirectory = " + outputDirectory);
-			getLog().debug("includes = " + Arrays.toString(includes));
-			getLog().debug("excludes = " + Arrays.toString(excludes));
-			getLog().debug("force = " + force);
-			getLog().debug("lessJs = " + lessJs);
-		}
+    if (getLog().isDebugEnabled()) {
+      getLog().debug("sourceDirectory = " + sourceDirectory);
+      getLog().debug("outputDirectory = " + outputDirectory);
+      getLog().debug("includes = " + Arrays.toString(includes));
+      getLog().debug("excludes = " + Arrays.toString(excludes));
+      getLog().debug("force = " + force);
+      getLog().debug("lessJs = " + lessJs);
+    }
 
-		String[] files = getIncludedFiles();
+    String[] files = getIncludedFiles();
 
-		if (files == null || files.length < 1) {
-			getLog().info("Nothing to compile - no LESS sources found");
-		} else {
-			if (getLog().isDebugEnabled()) {
-				getLog().debug("included files = " + Arrays.toString(files));
-			}
+    if (files == null || files.length < 1) {
+      getLog().info("Nothing to compile - no LESS sources found");
+    } else {
+      if (getLog().isDebugEnabled()) {
+        getLog().debug("included files = " + Arrays.toString(files));
+      }
 
-			Object lessCompiler = initLessCompiler();
-			try {
-				for (String file : files) {
-					File input = new File(sourceDirectory, file);
-	
-					buildContext.removeMessages(input);
-	
-					File output = new File(outputDirectory, file.replace(".less", ".css"));
-	
-					if (!output.getParentFile().exists() && !output.getParentFile().mkdirs()) {
-						throw new MojoExecutionException("Cannot create output directory " + output.getParentFile());
-					}
-	
-					try {
-						LessSource lessSource = new LessSource(input);
-	
-						if (output.lastModified() < lessSource.getLastModifiedIncludingImports() || force) {
-							getLog().info("Compiling LESS source: " + file + "...");
-							if (lessCompiler instanceof LessCompiler) {
-								((LessCompiler) lessCompiler).compile(lessSource, output, force);
-							} else {
-								((NodeJsLessCompiler) lessCompiler).compile(lessSource, output, force);
-							}
-							buildContext.refresh(output);
-						}
-						else {
-							getLog().info("Bypassing LESS source: " + file + " (not modified)");
-						}
-					} catch (IOException e) {
-						buildContext.addMessage(input, 0, 0, "Error compiling LESS source", BuildContext.SEVERITY_ERROR, e);
-						throw new MojoExecutionException("Error while compiling LESS source: " + file, e);
-					} catch (LessException e) {
-						String message = e.getMessage();
-						if (StringUtils.isEmpty(message)) {
-							message = "Error compiling LESS source";
-						}
-						buildContext.addMessage(input, 0, 0, "Error compiling LESS source", BuildContext.SEVERITY_ERROR, e);
-						throw new MojoExecutionException("Error while compiling LESS source: " + file, e);
-					} catch (InterruptedException e) {
-						buildContext.addMessage(input, 0, 0, "Error compiling LESS source", BuildContext.SEVERITY_ERROR, e);
-						throw new MojoExecutionException("Error while compiling LESS source: " + file, e);
-					}
-				}
-			} finally {
-				if (lessCompiler instanceof NodeJsLessCompiler) {
-					((NodeJsLessCompiler) lessCompiler).close();
-				}
-			}
+      Object lessCompiler = initLessCompiler();
+      try {
+        for (String file : files) {
+          File input = new File(sourceDirectory, file);
 
-			getLog().info("Compilation finished in " + (System.currentTimeMillis() - start) + " ms");
-		}
-	}
+          buildContext.removeMessages(input);
 
-	private Object initLessCompiler() throws MojoExecutionException {
-		if (nodeExecutable != null) {
-			NodeJsLessCompiler lessCompiler;
-			try {
-				lessCompiler = new NodeJsLessCompiler(compress, encoding, getLog());
-			} catch (IOException e) {
-				throw new MojoExecutionException(e.getMessage(), e);
-			}
-			if (lessJs != null) {
-				throw new MojoExecutionException(
-						"Custom LESS JavaScript is not currently supported when using nodeExecutable");
-			}
-			return lessCompiler;
-		} else {
-			LessCompiler lessCompiler = new LessCompiler();
-			lessCompiler.setCompress(compress);
-			lessCompiler.setEncoding(encoding);
-			if (lessJs != null) {
-				try {
-					lessCompiler.setLessJs(lessJs.toURI().toURL());
-				} catch (MalformedURLException e) {
-					throw new MojoExecutionException(
-							"Error while loading LESS JavaScript: " + lessJs.getAbsolutePath(), e);
-				}
-			}
-			return lessCompiler;
-		}
-	}
+          File output = new File(outputDirectory, file.replace(".less", ".css"));
+
+          if (!output.getParentFile().exists() && !output.getParentFile().mkdirs()) {
+            throw new MojoExecutionException("Cannot create output directory " + output.getParentFile());
+          }
+
+          try {
+            LessSource lessSource = new LessSource(sourceDirectory, input);
+
+            if (output.lastModified() < lessSource.getLastModifiedIncludingImports() || force) {
+              getLog().info("Compiling LESS source: " + file + "...");
+              if (lessCompiler instanceof LessCompiler) {
+                ((LessCompiler) lessCompiler).compile(lessSource, output, force);
+              } else {
+                ((NodeJsLessCompiler) lessCompiler).compile(lessSource, output, force);
+              }
+              buildContext.refresh(output);
+            } else {
+              getLog().info("Bypassing LESS source: " + file + " (not modified)");
+            }
+          } catch (IOException e) {
+            buildContext.addMessage(input, 0, 0, "Error compiling LESS source", BuildContext.SEVERITY_ERROR, e);
+            throw new MojoExecutionException("Error while compiling LESS source: " + file, e);
+          } catch (LessException e) {
+            String message = e.getMessage();
+            if (StringUtils.isEmpty(message)) {
+              message = "Error compiling LESS source";
+            }
+            buildContext.addMessage(input, 0, 0, "Error compiling LESS source", BuildContext.SEVERITY_ERROR, e);
+            throw new MojoExecutionException("Error while compiling LESS source: " + file, e);
+          } catch (InterruptedException e) {
+            buildContext.addMessage(input, 0, 0, "Error compiling LESS source", BuildContext.SEVERITY_ERROR, e);
+            throw new MojoExecutionException("Error while compiling LESS source: " + file, e);
+          }
+        }
+      } finally {
+        if (lessCompiler instanceof NodeJsLessCompiler) {
+          ((NodeJsLessCompiler) lessCompiler).close();
+        }
+      }
+
+      getLog().info("Compilation finished in " + (System.currentTimeMillis() - start) + " ms");
+    }
+  }
+
+  private Object initLessCompiler() throws MojoExecutionException {
+    if (nodeExecutable != null) {
+      NodeJsLessCompiler lessCompiler;
+      try {
+        lessCompiler = new NodeJsLessCompiler(compress, encoding, getLog());
+      } catch (IOException e) {
+        throw new MojoExecutionException(e.getMessage(), e);
+      }
+      if (lessJs != null) {
+        throw new MojoExecutionException("Custom LESS JavaScript is not currently supported when using nodeExecutable");
+      }
+      return lessCompiler;
+    } else {
+      LessCompiler lessCompiler = new LessCompiler();
+      lessCompiler.setCompress(compress);
+      lessCompiler.setEncoding(encoding);
+      if (lessJs != null) {
+        try {
+          lessCompiler.setLessJs(lessJs.toURI().toURL());
+        } catch (MalformedURLException e) {
+          throw new MojoExecutionException("Error while loading LESS JavaScript: " + lessJs.getAbsolutePath(), e);
+        }
+      }
+      return lessCompiler;
+    }
+  }
 }
